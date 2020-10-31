0d7e25ac6edbe5a34b30e9864304d71d843c60bd
99002557 <71481173+99002557@users.noreply.github.com>
2020-10-31 16:42:33 +0530

    Add files via upload

---
+++ Question-2/spec/firstspec.js
@@ -0,0 +1,38 @@
+describe('To test customer', () => {
+    let cust = null;
+    it('check name is of valid format', () => {
+        const cust = new customer("Neema", "neema@gmail.com" , "11-12-2020","1234567890","850");
+        expect(cust.name).toMatch("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");        
+        
+    });
+ 
+    it('should check name has min 5 characters', () => {
+        const cust = new customer("Neema", "neema@gmail.com" , "11-12-2020","1234567890","850");
+        expect(cust.name.length).toBe(5);
+        
+    });
+   it('should check mail is proper', () => {
+    const cust = new customer("Neema", "neema@gmail.com" , "11-12-2020","1234567890","850");
+     
+        expect(cust.email).toMatch("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");    
+    
+        
+    });
+    it('should check phone number', () => {
+        const cust = new customer("Neema", "neema@gmail.com" , "11-12-2020","1234567890","850");
+        expect(cust.phone.length).toBe(10);
+        
+    });
+   it('should check bill amount', () => {
+    const cust = new customer("Neema", "neema@gmail.com" , "11-12-2020","1234567890","850");
+    
+        expect(cust.billAmount).toBe('850');
+        
+    });
+    it('should check date', () => {
+        const cust = new customer("Neema", "neema@gmail.com" , "11-12-2020","1234567890","850");
+        
+            expect(cust.billDate).toBe('11-12-2020');
+            
+        });
+});
